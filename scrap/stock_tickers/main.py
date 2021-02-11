b9e5af35104938d91537f5c577a2c1dddc35f41b
Daniel <danieltujo@lewisu.edu>
2021-02-11 15:46:22 -0600

    Added alternate version of each ticker to include $ at the beginning

--- scrap/stock_tickers/main.py
+++ scrap/stock_tickers/main.py
@@ -2,10 +2,22 @@ import pandas as pd
 
 df = pd.read_csv("tickers.csv")
 ticker_list = df['DDD'].to_list()
-print(ticker_list)
+# print(ticker_list)
+
+
+
+ticker_list_with_sign = []
+for i in ticker_list:
+    ticker_list_with_sign.append(i)
+    temp_list = ["$", i]
+    ticker_string = "".join(temp_list)
+    ticker_list_with_sign.append(ticker_string)
+
+
+print(ticker_list_with_sign)
 
 amount = 0
-for i in ticker_list:
+for i in ticker_list_with_sign:
     amount += 1
 
 print(amount)
