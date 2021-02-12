46dabc35e4aba4a61fe1d3b476b145cef0d28d2e
Daniel <danieltujo@lewisu.edu>
2021-02-12 16:35:44 -0600

    Added three separate lists, Ticker, $ticker, and company name.
    Add get_symbol function that grabs company name for the symbol
    
    Created dataframe with three columns, ticker, $ticker, company name.
    If there "none" in the column spot for a ticker, it means that that ticker/company is not on the stock market anymore

--- scrap/stock_tickers/main.py
+++ scrap/stock_tickers/main.py
@@ -1,23 +1,34 @@
 import pandas as pd
+import requests
+from pandas import DataFrame
 
 df = pd.read_csv("tickers.csv")
 ticker_list = df['DDD'].to_list()
-# print(ticker_list)
-
 
 
 ticker_list_with_sign = []
+ticker_name = []
+
+
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == symbol:
+            return x['name']
+
+
 for i in ticker_list:
-    ticker_list_with_sign.append(i)
     temp_list = ["$", i]
     ticker_string = "".join(temp_list)
     ticker_list_with_sign.append(ticker_string)
+    company = get_symbol(i)
+    print(company)
+    ticker_name.append(company)
+
+df = pd.DataFrame({'Ticker': ticker_list, '$Ticker': ticker_list_with_sign, 'Company Name': ticker_name})
+df.to_csv(r'D:\Git\lewisuDataSAIL\Dataframes\tickertable.csv', index=False)
 
 
-print(ticker_list_with_sign)
 
-amount = 0
-for i in ticker_list_with_sign:
-    amount += 1
-
-print(amount)
