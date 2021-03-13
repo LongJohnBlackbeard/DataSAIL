f01528191c19defe8c98227be6c6970641b56e34
Daniel Tujo <tujo1992@gmail.com>
2021-03-13 08:49:38 -0600

    update error handling

--- scrap/finance_test/main.py
+++ scrap/finance_test/main.py
@@ -4,36 +4,27 @@ import pandas as pd
 import time
 import datetime as dt
 import numpy as np
+import sys
 
 tickers = pd.read_csv('tickertable.csv')
 ts = TimeSeries(key='09150caf98mshdbd66c92f94bae1p1d9a46jsn48251394fdde', output_format="pandas")
 
 arr = tickers['Tickers'].to_numpy()
 
-
 counter = 0
-for tick in arr:
-    counter +=1
-    daily_data, meta_data = ts.get_daily(symbol=tick, outputsize='full')
-    daily_data.to_csv(r'D:\Git\lewisuDataSAIL\Dataframes\Stock Data\%s.csv' % tick)
-    arr = np.delete(arr, np.where(arr == tick))
-    time.sleep(12)
-    if counter == 450:
-        break
+try:
+    for tick in arr:
+        counter += 1
+        daily_data, meta_data = ts.get_daily(symbol=tick, outputsize='full')
+        daily_data.to_csv(r'D:\Git\lewisuDataSAIL\Dataframes\Stock Data\%s.csv' % tick)
+        arr = np.delete(arr, np.where(arr == tick))
+        time.sleep(12)
+        if counter == 450:
+            break
+except:
+    e = sys.exc_info()[0]
+    print('ERROR: ', e)
+
 
 tickers_remaining = pd.DataFrame({'Tickers': arr})
 tickers_remaining.to_csv(r'D:\Git\lewisuDataSAIL\scrap\finance_test\tickertable.csv')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
