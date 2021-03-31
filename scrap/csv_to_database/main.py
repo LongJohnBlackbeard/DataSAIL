ebf004419ac9ed50624daba078c49fa977d38dfb
Daniel Tujo <tujo1992@gmail.com>
2021-03-31 10:20:52 -0500

    testing

--- scrap/csv_to_database/main.py
+++ scrap/csv_to_database/main.py
@@ -1,8 +1,18 @@
 import os
+import pandas as pd
+import mysql.connector
+
+cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
+myCursor = cnx.cursor()
 
 directory = r'/home/dtujo/myoptane/Trawler/Dataframes'
 for filename in os.listdir(directory):
     if filename.endswith(".csv"):
         print(os.path.join(directory, filename))
+        data = pd.read_csv(filename)
+        for i in range(0, len(data.columns)):
+            print(i)
+
+        myCursor.execute()
     else:
         continue
