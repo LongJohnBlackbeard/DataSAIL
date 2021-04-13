82ad121ba9969df7488fb36d57b675cdad00935d
Daniel Tujo <tujo1992@gmail.com>
2021-04-13 13:42:04 -0500

    testing ap v8

--- scrap/historicalStockToDB/main.py
+++ scrap/historicalStockToDB/main.py
@@ -21,32 +21,33 @@ ts = timeseries.TimeSeries(key='DEO17X8J2DIV6483', output_format='pandas')
 
 arr = tickers['Tickers'].to_numpy()
 
-daily_data, meta_data = ts.get_daily(symbol="A", outputsize='full')
+for ticker in arr:
+    daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
 
-daily_data = daily_data.reset_index()
-# print(daily_data)
+    daily_data = daily_data.reset_index()
+    # print(daily_data)
 
-start_date = datetime(2021, 4, 12)
-# print(daily_data.date.iat[-1])
+    start_date = datetime(2021, 4, 12)
+    # print(daily_data.date.iat[-1])
 
-dateRange = pd.date_range(start=daily_data.date.iat[-1], end=start_date, freq='D')[::-1]
-# print(dateRange)
+    dateRange = pd.date_range(start=daily_data.date.iat[-1], end=start_date, freq='D')[::-1]
+    # print(dateRange)
 
-dateRangeDF = pd.DataFrame(index=dateRange)
-dateRangeDF.reset_index(inplace=True)
-print(dateRangeDF)
-temp_list = ['date']
-dateRangeDF.columns = temp_list
+    dateRangeDF = pd.DataFrame(index=dateRange)
+    dateRangeDF.reset_index(inplace=True)
+    print(dateRangeDF)
+    temp_list = ['date']
+    dateRangeDF.columns = temp_list
 
-dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
+    dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
 
-print(dailyDataFinal)
-dailyDataFinal.to_csv('test.csv')
+    print(dailyDataFinal)
+    dailyDataFinal.to_csv('test.csv')
 
-for i, row in dailyDataFinal.iterrows():
-    sql = "INSERT INTO `Trawler` (date, open, high, low, close, volume, stock) VALUES (" + "%s," * (len(row) - 1) + "%s, 'A')"
-    cursor.execute(sql, tuple(row))
+    for i, row in dailyDataFinal.iterrows():
+        sql = "INSERT INTO `Trawler` (date, open, high, low, close, volume, stock) VALUES (" + "%s," * (len(row) - 1) + "%s, 'A')"
+        cursor.execute(sql, tuple(row))
 
-    connection.commit()
+        connection.commit()
 
-connection.close()
+    connection.close()
