83ac2916c4a9f43874208ef19af40105f7eaa5da
Daniel Tujo <tujo1992@gmail.com>
2021-04-13 19:33:55 -0500

    testing ap v54

--- scrap/historicalStockToDB/main.py
+++ scrap/historicalStockToDB/main.py
@@ -15,7 +15,7 @@ import mysql.connector
 cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
 myCursor = cnx.cursor()
 
-tickers = pd.read_csv('tickertable.csv')
+tickers = pd.read_csv('FinalTickerTable.csv')
 ts = timeseries.TimeSeries(key='DEO17X8J2DIV6483', output_format='pandas')
 # ts = TimeSeries(key='DEO17X8J2DIV6483', output_format='pandas')
 
@@ -24,8 +24,6 @@ counter = 0
 
 for ticker in arr:
     print(ticker)
-
-    time.sleep(.6)
 
     daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
 
@@ -59,17 +57,17 @@ for ticker in arr:
             values_list = [str(dailyDataFinal.loc[i]['date']), dailyDataFinal.loc[i]['1. open'],
                            dailyDataFinal.loc[i]['2. high'], dailyDataFinal.loc[i]['3. low'],
                            dailyDataFinal.loc[i]['4. close'], int(dailyDataFinal.loc[i]['5. volume'])]
-        print(values_list)
+        # print(values_list)
 
         if len(values_list) == 6:
             values_list.append(ticker)
-            print("Add Ticker Row: ", values_list)
-            print(tuple(values_list))
+            # print("Add Ticker Row: ", values_list)
+            # print(tuple(values_list))
             myCursor.execute(sql, tuple(values_list))
         else:
             addedValues = [0, 0, 0, 0, 0, ticker]
             values_list.append(addedValues)
-            print("Null row: ", values_list)
+            # print("Null row: ", values_list)
             myCursor.execute(sql, tuple(values_list))
 
         cnx.commit()
