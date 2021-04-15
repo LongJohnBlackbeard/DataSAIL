dca0e7273d60429be87d3d583b568ec089adcef2
Daniel Tujo <tujo1992@gmail.com>
2021-04-15 11:02:20 -0500

    trying try catch

--- scrap/historicalStockToDB/main.py
+++ scrap/historicalStockToDB/main.py
@@ -25,58 +25,64 @@ fullArr = fullTickers['Ticker'].to_numpy()
 
 arr = []
 for ticker in fullArr:
-    if ticker not in semiArr:
+    if (ticker not in semiArr) and ("." not in ticker):
         arr.append(ticker)
 counter = 0
 
-for ticker in arr:
-    print(ticker)
+try:
+    for ticker in arr:
+        print(ticker)
 
-    daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
+        daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
 
-    daily_data = daily_data.reset_index()
-    # print(daily_data)
+        daily_data = daily_data.reset_index()
+        # print(daily_data)
 
-    start_date = datetime(2021, 4, 12)
-    # print(daily_data.date.iat[-1])
+        start_date = datetime(2021, 4, 12)
+        # print(daily_data.date.iat[-1])
 
-    dateRange = pd.date_range(start=daily_data.date.iat[-1], end=start_date, freq='D')[::-1]
-    # print(dateRange)
+        dateRange = pd.date_range(start=daily_data.date.iat[-1], end=start_date, freq='D')[::-1]
+        # print(dateRange)
 
-    dateRangeDF = pd.DataFrame(index=dateRange)
-    dateRangeDF.reset_index(inplace=True)
-    # print(dateRangeDF)
-    temp_list = ['date']
-    dateRangeDF.columns = temp_list
+        dateRangeDF = pd.DataFrame(index=dateRange)
+        dateRangeDF.reset_index(inplace=True)
+        # print(dateRangeDF)
+        temp_list = ['date']
+        dateRangeDF.columns = temp_list
 
-    dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
+        dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
 
-    # print(dailyDataFinal)
-    dailyDataFinal.to_csv('test.csv')
+        # print(dailyDataFinal)
+        dailyDataFinal.to_csv('test.csv')
 
-    row_count = len(dailyDataFinal.index)
+        row_count = len(dailyDataFinal.index)
 
-    for i in range(0, row_count):
-        sql = "INSERT INTO Trawler (date, open, high, low, close, volume, stock) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        if pd.isnull(dailyDataFinal.loc[i]['5. volume']):
-            values_list = [str(dailyDataFinal.loc[i]['date']), 0, 0, 0, 0, 0]
-        else:
-            values_list = [str(dailyDataFinal.loc[i]['date']), dailyDataFinal.loc[i]['1. open'],
-                           dailyDataFinal.loc[i]['2. high'], dailyDataFinal.loc[i]['3. low'],
-                           dailyDataFinal.loc[i]['4. close'], int(dailyDataFinal.loc[i]['5. volume'])]
-        # print(values_list)
+        for i in range(0, row_count):
+            sql = "INSERT INTO Trawler (date, open, high, low, close, volume, stock) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            if pd.isnull(dailyDataFinal.loc[i]['5. volume']):
+                values_list = [str(dailyDataFinal.loc[i]['date']), 0, 0, 0, 0, 0]
+            else:
+                values_list = [str(dailyDataFinal.loc[i]['date']), dailyDataFinal.loc[i]['1. open'],
+                               dailyDataFinal.loc[i]['2. high'], dailyDataFinal.loc[i]['3. low'],
+                               dailyDataFinal.loc[i]['4. close'], int(dailyDataFinal.loc[i]['5. volume'])]
+            # print(values_list)
 
-        if len(values_list) == 6:
-            values_list.append(ticker)
-            # print("Add Ticker Row: ", values_list)
-            # print(tuple(values_list))
-            myCursor.execute(sql, tuple(values_list))
-        else:
-            addedValues = [0, 0, 0, 0, 0, ticker]
-            values_list.append(addedValues)
-            # print("Null row: ", values_list)
-            myCursor.execute(sql, tuple(values_list))
+            if len(values_list) == 6:
+                values_list.append(ticker)
+                # print("Add Ticker Row: ", values_list)
+                # print(tuple(values_list))
+                myCursor.execute(sql, tuple(values_list))
+            else:
+                addedValues = [0, 0, 0, 0, 0, ticker]
+                values_list.append(addedValues)
+                # print("Null row: ", values_list)
+                myCursor.execute(sql, tuple(values_list))
 
-        cnx.commit()
+            cnx.commit()
+except Exception as inst:
+    print("Exception Occured: ")
+    print(type(inst))
+    print(inst.args)
+    print(inst)
 
 cnx.close()
