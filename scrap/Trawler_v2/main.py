2cc8219625c018ce1a8458eac856c79a962fe5e2
Daniel Tujo <tujo1992@gmail.com>
2021-04-22 09:28:19 -0500

    testing counts to db v7

--- scrap/Trawler_v2/main.py
+++ scrap/Trawler_v2/main.py
@@ -5,14 +5,17 @@ import grabPosts
 import redditInstance
 import pandas as pd
 import mysql.connector
+from datetime import datetime
 
 # auth = redditInstance.initiate_instance()
 #
 # dataDF = grabPosts.post_and_timestamps(auth, grabPosts.reddit_input_lists)
 dataDF = pd.read_csv(r'/home/dtujo/myoptane/Trawler/Dataframes/Stocks_03-13-2021.csv')
+print("READ CSV FILE")
 
 data = ''.join(dataDF['Post/Comment'])
-print(data)
+print("CONCATENATED POSTS AND COMMENTS")
+
 
 result = findCounts.process_bodies(data)
 
@@ -20,27 +23,31 @@ result = findCounts.filter_pos_tokens(result, findCounts.target_pos_tags)
 
 result = findCounts.count_tickers(result, findCounts.tickers)
 
-print(type(result))
+print("TICKER COUNTS FOUND")
 
-print(result)
+
 resultDF = pd.DataFrame(list(result.items()), columns=['Ticker', 'Count'])
-
-print(resultDF)
+print("TICKER COUNTS IN DATAFRAME")
 
 # resultDF.to_csv(r'D:\Git\lewisuDataSAIL\Dataframes\testing.csv', index=False)
 
 cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
 myCursor = cnx.cursor()
+print("CONNECTION TO DB MADE")
 
 row_count = len(resultDF.index)
 
 tickerList = resultDF['Ticker'].tolist()
 countList = resultDF['Count'].tolist()
+dateList = dataDF['Timestamp'].tolist()
+dateFix = datetime.strptime(dateList[1], "%m/%d/%Y")
+print("VALUES SAVED TO LIST")
 
 for i in range(0, row_count):
     sql = "Update testingTrawler SET count = %s WHERE date = %s AND stock = %s"
-    val = (countList[i], dataDF[1]['Timestamp'], tickerList[i])
+    val = (countList[i], dateFix, tickerList[i])
     myCursor.execute(sql, val)
     cnx.commit()
+    print("ROW UPDATED # %d" % i)
 
 cnx.close()
