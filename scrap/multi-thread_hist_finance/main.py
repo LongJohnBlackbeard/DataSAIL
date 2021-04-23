551e0bda04df5f3dabe9a41dc3a692a5901cbc29
tujo1992@gmail.com <tujo1992@gmail.com>
2021-04-23 14:48:25 -0400

    update 4-21

--- scrap/multi-thread_hist_finance/main.py
+++ scrap/multi-thread_hist_finance/main.py
@@ -38,58 +38,67 @@ for tick in arr:
 
 
 def data_grab_send(ticker):
-    sleep(30)
-    # mysql connection
-    cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
-    myCursor = cnx.cursor()
+    
+    try:
+        # mysql connection
+        cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
+        myCursor = cnx.cursor()
 
-    print("Starting %s" % ticker)
-    tic1 = time.perf_counter()
-    # grabs data from alpha-vantage
-    daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
-    # reset index of the pd
-    daily_data = daily_data.reset_index()
-    # creates a date range to grab data and fill in missing dates
-    start_date = datetime(2021, 4, 20)
-    dateRange = pd.date_range(start=daily_data.date.iat[-1], end=start_date, freq='D')[::-1]
-    # sends daterange to pd and resets index
-    dateRangeDF = pd.DataFrame(index=dateRange)
-    dateRangeDF.reset_index(inplace=True)
-    # adding new column
-    temp_list = ['date']
-    dateRangeDF.columns = temp_list
-    # merging two df
-    dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
-    # creates a count of dates and saves to a variable
-    row_count = len(dailyDataFinal.index)
+        print("Starting %s" % ticker, flush=True)
+        sleep(30)
+        tic1 = time.perf_counter()
+        # grabs data from alpha-vantage
+        daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
+        # reset index of the pd
+        daily_data = daily_data.reset_index()
+        # creates a date range to grab data and fill in missing dates
+        start_date = datetime(2021, 4, 23)
+        dateRange = pd.date_range(start=daily_data.date.iat[-1], end=start_date, freq='D')[::-1]
+        # sends daterange to pd and resets index
+        dateRangeDF = pd.DataFrame(index=dateRange)
+        dateRangeDF.reset_index(inplace=True)
+        # adding new column
+        temp_list = ['date']
+        dateRangeDF.columns = temp_list
+        # merging two df
+        dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
+        # creates a count of dates and saves to a variable
+        row_count = len(dailyDataFinal.index)
 
-    # Loop through every date and grabs data and sends to database
-    for i in range(0, row_count):
-        sql = "INSERT INTO testingTrawler2 (date, open, high, low, close, volume, stock) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        if pd.isnull(dailyDataFinal.loc[i]['5. volume']):
-            values_list = [str(dailyDataFinal.loc[i]['date']), 0, 0, 0, 0, 0]
-        else:
-            values_list = [str(dailyDataFinal.loc[i]['date']), dailyDataFinal.loc[i]['1. open'],
-                        dailyDataFinal.loc[i]['2. high'], dailyDataFinal.loc[i]['3. low'],
-                        dailyDataFinal.loc[i]['4. close'], int(dailyDataFinal.loc[i]['5. volume'])]
-        print(values_list)
+        # Loop through every date and grabs data and sends to database
+        for i in range(0, row_count):
+            sql = "INSERT INTO testingTrawler2 (date, open, high, low, close, volume, stock) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            if pd.isnull(dailyDataFinal.loc[i]['5. volume']):
+                values_list = [str(dailyDataFinal.loc[i]['date']), 0, 0, 0, 0, 0]
+            else:
+                values_list = [str(dailyDataFinal.loc[i]['date']), dailyDataFinal.loc[i]['1. open'],
+                            dailyDataFinal.loc[i]['2. high'], dailyDataFinal.loc[i]['3. low'],
+                            dailyDataFinal.loc[i]['4. close'], int(dailyDataFinal.loc[i]['5. volume'])]
+            # print(values_list)
 
-        if len(values_list) == 6:
-            values_list.append(ticker)
-            # print("Add Ticker Row: ", values_list)
-            # print(tuple(values_list))
-            myCursor.execute(sql, tuple(values_list))
-        else:
-            addedValues = [0, 0, 0, 0, 0, ticker]
-            values_list.append(addedValues)
-            # print("Null row: ", values_list)
-            myCursor.execute(sql, tuple(values_list))
+            if len(values_list) == 6:
+                values_list.append(ticker)
+                # print("Add Ticker Row: ", values_list)
+                # print(tuple(values_list))
+                myCursor.execute(sql, tuple(values_list))
+            else:
+                addedValues = [0, 0, 0, 0, 0, ticker]
+                values_list.append(addedValues)
+                # print("Null row: ", values_list)
+                myCursor.execute(sql, tuple(values_list))
+        toc1 = time.perf_counter()
+        done = ("%s finished in %0.4f seconds" % (ticker, (toc1 - tic1)))
+        print(done, flush=True)
 
-    cnx.commit()
-    cnx.close()
-    toc1 = time.perf_counter()
-    done = ("%s finished in %0.4f seconds" % (ticker, (toc1 - tic1)))
-    print(done)
+        cnx.commit()
+        cnx.close()
+    except ValueError:
+        print("For stock: ", ticker, " ", ValueError, flush=True)
+        pass
+
+
+    
+    
 
 
 # multi-threading
@@ -120,10 +129,11 @@ def data_grab_send(ticker):
 tic = time.perf_counter()
 
 with Parallel(n_jobs=50) as parallel:
-    print(parallel([delayed(data_grab_send)(i) for i in arr]))
+    print(parallel([delayed(data_grab_send)(i) for i in arr]), flush=True)
 
 toc = time.perf_counter()
-print("finished in %0.4f seconds" % (toc - tic))
+print("Total time: %0.4f" % (toc - tic))
+
 
 # regular
 
