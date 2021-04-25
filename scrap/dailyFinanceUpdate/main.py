fbce8c6c68bc3d850050f447aa9a47d48f0cd9df
Daniel Tujo <tujo1992@gmail.com>
2021-04-25 08:05:00 -0500

    testing select query, testing date formatting

--- scrap/dailyFinanceUpdate/main.py
+++ scrap/dailyFinanceUpdate/main.py
@@ -3,6 +3,7 @@ from time import sleep
 from joblib import Parallel, delayed
 import pandas as pd
 import mysql.connector
+from datetime import datetime
 
 cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
 
@@ -10,8 +11,10 @@ myCursor = cnx.cursor()
 
 myCursor.execute("SELECT date FROM testingTrawler ORDER BY date DESC LIMIT  1")
 records = myCursor.fetchall()
-print(type(records))
-print(records)
+
+dateString = datetime.strftime(records[0], "%Y-%m-%d")
+print(type(dateString))
+print(dateString)
 
 myCursor.close()
 
