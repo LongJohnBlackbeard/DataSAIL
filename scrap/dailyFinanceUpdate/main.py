832e797a61a956767eb3d8c9f362954c02bb9924
Daniel Tujo <tujo1992@gmail.com>
2021-04-25 07:56:38 -0500

    init commit

---
+++ scrap/dailyFinanceUpdate/main.py
@@ -0,0 +1,16 @@
+import time
+from time import sleep
+from joblib import Parallel, delayed
+import pandas as pd
+import mysql.connector
+
+cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
+
+myCursor = cnx.cursor()
+
+recentDate = myCursor.execute("SELECT date, FROM testingTraweler1 ORDER BY timestamp DESC LIMIT  1")
+
+print(recentDate)
+
+
+
