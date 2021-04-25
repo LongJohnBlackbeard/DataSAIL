a08b27425eb9a7983b199194a36b94b3fb401a9a
Daniel Tujo <tujo1992@gmail.com>
2021-04-25 08:06:22 -0500

    testing select query, testing date formatting, fixing tuple

--- scrap/dailyFinanceUpdate/main.py
+++ scrap/dailyFinanceUpdate/main.py
@@ -11,10 +11,11 @@ myCursor = cnx.cursor()
 
 myCursor.execute("SELECT date FROM testingTrawler ORDER BY date DESC LIMIT  1")
 records = myCursor.fetchall()
+dateTuple = records[1]
 
-dateString = datetime.strftime(records[0], "%Y-%m-%d")
-print(type(dateString))
-print(dateString)
+# dateString = datetime.strftime(records[0], "%Y-%m-%d")
+# print(type(dateString))
+# print(dateString)
 
 myCursor.close()
 
