251c006a9060222ad0b96301ccb7d418daf30720
Daniel Tujo <tujo1992@gmail.com>
2021-04-25 11:44:23 -0500

    trying try/except

--- scrap/Trawler_v2/grabFinance.py
+++ scrap/Trawler_v2/grabFinance.py
@@ -10,79 +10,83 @@ import datetime
 from alpha_vantage.alpha_vantage import timeseries
 
 def grabFinance():
-    ts = timeseries.TimeSeries(key='DEO17X8J2DIV6483', output_format='pandas')
-    nasdaq = pd.read_csv('nasdaqlist.csv')
-    nasdaqTick = nasdaq['Ticker'].to_numpy()
-    arr = []
-    for tick in nasdaqTick:
-        if tick not in arr:
-            arr.append(tick)
+    try:
+        ts = timeseries.TimeSeries(key='DEO17X8J2DIV6483', output_format='pandas')
+        nasdaq = pd.read_csv('nasdaqlist.csv')
+        nasdaqTick = nasdaq['Ticker'].to_numpy()
+        arr = []
+        for tick in nasdaqTick:
+            if tick not in arr:
+                arr.append(tick)
 
-    for tick in arr:
-        if ("." in tick) or ("-" in tick):
-            arr.remove(tick)
+        for tick in arr:
+            if ("." in tick) or ("-" in tick):
+                arr.remove(tick)
 
-    cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
-    myCursor = cnx.cursor()
-
-    myCursor.execute("SELECT date FROM testingTrawler ORDER BY date DESC LIMIT  1")
-    records = myCursor.fetchall()
-    dateTuple = records[0]
-    dateObject = dateTuple[0]
-
-    startDate = dateObject + datetime.timedelta(days=1)
-    endDate = datetime.datetime.today()
-    endDate = endDate - datetime.timedelta(days=1)
-    dateRange = pd.date_range(start=startDate, end=endDate, freq="D")[::-1]
-
-    dateRangeDF = pd.DataFrame(index=dateRange)
-    dateRangeDF.reset_index(inplace=True)
-    temp_list = ['date']
-    dateRangeDF.columns = temp_list
-
-    print(dateRangeDF)
-    dateRangeList = dateRangeDF['date'].to_list()
-    print(dateRangeList)
-    cnx.close()
-
-    def dataGrabSend(ticker):
-        sleep(30)
         cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
         myCursor = cnx.cursor()
 
-        daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='compact')
-        daily_data = daily_data.reset_index()
+        myCursor.execute("SELECT date FROM testingTrawler ORDER BY date DESC LIMIT  1")
+        records = myCursor.fetchall()
+        dateTuple = records[0]
+        dateObject = dateTuple[0]
 
-        dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
+        startDate = dateObject + datetime.timedelta(days=1)
+        endDate = datetime.datetime.today()
+        endDate = endDate - datetime.timedelta(days=1)
+        dateRange = pd.date_range(start=startDate, end=endDate, freq="D")[::-1]
 
-        for index, row in dailyDataFinal.iterrows():
-            if row['date'] in dateRangeList:
-                sql = "INSERT INTO testingTrawler (date, open, high, low, close, volume, stock) " \
-                      "VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                if pd.isnull(row['5. volume']):
-                    values_list = [row['date'], 0, 0, 0, 0, 0]
-                else:
-                    values_list = [row['date'], row['1. open'], row['2. high'], row['3. low'], row['4. close'],
-                                   row['5. volume']]
-                if len(values_list) == 6:
-                    values_list.append(ticker)
-                    # print("Add Ticker Row: ", values_list)
-                    # print(tuple(values_list))
-                    myCursor.execute(sql, tuple(values_list))
-                else:
-                    addedValues = [0, 0, 0, 0, 0, ticker]
-                    values_list.append(addedValues)
-                    # print("Null row: ", values_list)
-                    myCursor.execute(sql, tuple(values_list))
+        dateRangeDF = pd.DataFrame(index=dateRange)
+        dateRangeDF.reset_index(inplace=True)
+        temp_list = ['date']
+        dateRangeDF.columns = temp_list
 
-                cnx.commit()
-        print(ticker, " executed")
+        print(dateRangeDF)
+        dateRangeList = dateRangeDF['date'].to_list()
+        print(dateRangeList)
         cnx.close()
 
-    tic = time.perf_counter()
+        def dataGrabSend(ticker):
+            sleep(30)
+            cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
+            myCursor = cnx.cursor()
 
-    with Parallel(n_jobs=50) as parallel:
-        print(parallel([delayed(dataGrabSend)(i) for i in arr]), flush=True)
+            daily_data, meta_data = ts.get_daily(symbol=ticker, outputsize='compact')
+            daily_data = daily_data.reset_index()
 
-    toc = time.perf_counter()
-    print("Total time: %0.4f" % (toc - tic))
+            dailyDataFinal = daily_data.merge(dateRangeDF, how='outer', on='date')
+
+            for index, row in dailyDataFinal.iterrows():
+                if row['date'] in dateRangeList:
+                    sql = "INSERT INTO testingTrawler (date, open, high, low, close, volume, stock) " \
+                          "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                    if pd.isnull(row['5. volume']):
+                        values_list = [row['date'], 0, 0, 0, 0, 0]
+                    else:
+                        values_list = [row['date'], row['1. open'], row['2. high'], row['3. low'], row['4. close'],
+                                       row['5. volume']]
+                    if len(values_list) == 6:
+                        values_list.append(ticker)
+                        # print("Add Ticker Row: ", values_list)
+                        # print(tuple(values_list))
+                        myCursor.execute(sql, tuple(values_list))
+                    else:
+                        addedValues = [0, 0, 0, 0, 0, ticker]
+                        values_list.append(addedValues)
+                        # print("Null row: ", values_list)
+                        myCursor.execute(sql, tuple(values_list))
+
+                    cnx.commit()
+            print(ticker, " executed")
+            cnx.close()
+
+        tic = time.perf_counter()
+
+        with Parallel(n_jobs=50) as parallel:
+            print(parallel([delayed(dataGrabSend)(i) for i in arr]), flush=True)
+
+        toc = time.perf_counter()
+        print("Total time: %0.4f" % (toc - tic))
+    except Exception as e:
+        print("Error: ", e)
+
