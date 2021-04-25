6fead15ad2852549ed85d9948b0c4adc88ed0fff
Daniel Tujo <tujo1992@gmail.com>
2021-04-25 07:59:21 -0500

    testing select query v2

--- scrap/dailyFinanceUpdate/main.py
+++ scrap/dailyFinanceUpdate/main.py
@@ -8,7 +8,7 @@ cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhos
 
 myCursor = cnx.cursor()
 
-recentDate = myCursor.execute("SELECT date FROM testingTraweler ORDER BY date DESC LIMIT  1")
+recentDate = myCursor.execute("SELECT date FROM testingTrawler ORDER BY date DESC LIMIT  1")
 
 print(recentDate)
 
