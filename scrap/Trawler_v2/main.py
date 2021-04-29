216b8612501429c18d2d424ba89476b2d9503655
Daniel Tujo <tujo1992@gmail.com>
2021-04-29 07:23:44 -0500

    testing pipeline v19

--- scrap/Trawler_v2/main.py
+++ scrap/Trawler_v2/main.py
@@ -30,74 +30,78 @@ print("CONNECTING TO DB", flush=True)
 
 
 def runCountFinder(File):
-    tic = time.perf_counter()
-    cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
-    myCursor = cnx.cursor()
+    try:
+        tic = time.perf_counter()
+        cnx = mysql.connector.connect(user='dtujo', password='dtujo-mys', host='localhost', database='DataSAIL')
+        myCursor = cnx.cursor()
 
-    print("READING CSV FILE: %s" % File, flush=True)
-    dataDF = pd.read_csv(r'/home/dtujo/myoptane/Trawler/Dataframes/%s' % File)
-    # print("COMPLETED", flush=True)
+        print("READING CSV FILE: %s" % File, flush=True)
+        dataDF = pd.read_csv(r'/home/dtujo/myoptane/Trawler/Dataframes/%s' % File)
+        # print("COMPLETED", flush=True)
 
-    # print("CONCATENATING POSTS AND COMMENTS", flush=True)
-    data = ''.join(map(str, dataDF['Post/Comment']))
-    # print("COMPLETED", flush=True)
+        # print("CONCATENATING POSTS AND COMMENTS", flush=True)
+        data = ''.join(map(str, dataDF['Post/Comment']))
+        # print("COMPLETED", flush=True)
 
-    # print("RUNNING FIND COUNTS", flush=True)
-    result = findCounts.process_bodies(data)
+        # print("RUNNING FIND COUNTS", flush=True)
+        result = findCounts.process_bodies(data)
 
-    result = findCounts.filter_pos_tokens(result, findCounts.target_pos_tags)
+        result = findCounts.filter_pos_tokens(result, findCounts.target_pos_tags)
 
-    result = findCounts.count_tickers(result, findCounts.tickers)
+        result = findCounts.count_tickers(result, findCounts.tickers)
 
-    # print("COMPLETED", flush=True)
+        # print("COMPLETED", flush=True)
 
-    # print("TRANSFERRING TICKER COUNTS TO DATAFRAME", flush=True)
-    resultDF = pd.DataFrame(list(result.items()), columns=['Ticker', 'Count'])
-    print(File, "\n", resultDF, )
-    # print("COMPLETED", flush=True)
+        # print("TRANSFERRING TICKER COUNTS TO DATAFRAME", flush=True)
+        resultDF = pd.DataFrame(list(result.items()), columns=['Ticker', 'Count'])
+        print(File, "\n", resultDF, )
+        # print("COMPLETED", flush=True)
 
-    # resultDF.to_csv(r'D:\Git\lewisuDataSAIL\Dataframes\testing.csv', index=False)
+        # resultDF.to_csv(r'D:\Git\lewisuDataSAIL\Dataframes\testing.csv', index=False)
 
-    row_count = len(resultDF.index)
+        row_count = len(resultDF.index)
 
-    # print("SAVING VALUES TO LISTS", flush=True)
-    tickerList = resultDF['Ticker'].tolist()
-    countList = resultDF['Count'].tolist()
-    dateList = dataDF['Timestamp'].tolist()
-    print(dateList[1])
+        # print("SAVING VALUES TO LISTS", flush=True)
+        tickerList = resultDF['Ticker'].tolist()
+        countList = resultDF['Count'].tolist()
+        dateList = dataDF['Timestamp'].tolist()
+        print(dateList[1])
 
-    dateFix = dateList[1]
+        dateFix = dateList[1]
 
-    # if len(str(dateList[1])) == 10:
-    #     dateFix = datetime.strptime(dateList[1], "%m/%d/%Y")
-    # else:
-    #     date_slice = dateList[1]
-    #     date_slice = date_slice[0:10]
-    #     dateFix = datetime.strptime(date_slice, "%Y-%m-%d")
-    # print("COMPLETED", flush=True)
+        # if len(str(dateList[1])) == 10:
+        #     dateFix = datetime.strptime(dateList[1], "%m/%d/%Y")
+        # else:
+        #     date_slice = dateList[1]
+        #     date_slice = date_slice[0:10]
+        #     dateFix = datetime.strptime(date_slice, "%Y-%m-%d")
+        # print("COMPLETED", flush=True)
 
-    for i in range(0, row_count):
-        sql1 = "SELECT mentions FROM Trawler WHERE date = %s AND stock = %s"
-        val1 = (dateFix, tickerList[i])
-        myCursor.execute(sql1, val1)
-        dbMentionCount = myCursor.fetchone()
-        # print(tickerList[i])
-        # print(dbMentionCount)
+        for i in range(0, row_count):
+            sql1 = "SELECT mentions FROM Trawler WHERE date = %s AND stock = %s"
+            val1 = (dateFix, tickerList[i])
+            myCursor.execute(sql1, val1)
+            dbMentionCount = myCursor.fetchone()
+            # print(tickerList[i])
+            # print(dbMentionCount)
 
-        try:
-            # print("count(%d) + dbcount(%d)" % (countList[i], dbMentionCount[0]))
-            newCount = countList[i] + dbMentionCount[0]
-        except Exception:
-            continue
-        sql = "Update Trawler SET mentions = %s WHERE date = %s AND stock = %s"
-        val = (newCount, dateFix, tickerList[i])
-        myCursor.execute(sql, val)
-        cnx.commit()
-        # print("ROW UPDATED # %d" % i)
+            try:
+                # print("count(%d) + dbcount(%d)" % (countList[i], dbMentionCount[0]))
+                newCount = countList[i] + dbMentionCount[0]
+            except Exception:
+                continue
+            sql = "Update Trawler SET mentions = %s WHERE date = %s AND stock = %s"
+            val = (newCount, dateFix, tickerList[i])
+            myCursor.execute(sql, val)
+            cnx.commit()
+            # print("ROW UPDATED # %d" % i)
 
-    cnx.close()
-    toc = time.perf_counter()
-    print("%s Completed***** in %0.4f seconds" % (File, (toc - tic)), flush=True)
+        cnx.close()
+        toc = time.perf_counter()
+        print("%s Completed***** in %0.4f seconds" % (File, (toc - tic)), flush=True)
+    except Exception as e:
+        print(File)
+        print(Exception)
 
 
 # CSV PORTION #################
