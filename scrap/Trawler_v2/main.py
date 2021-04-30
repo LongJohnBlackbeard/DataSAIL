b82d6a5002e1525bea3f7bde70e6c7e92a1cce81
Daniel Tujo <tujo1992@gmail.com>
2021-04-29 22:42:15 -0500

    testing pipeline v34

--- scrap/Trawler_v2/main.py
+++ scrap/Trawler_v2/main.py
@@ -19,7 +19,7 @@ begin = time.perf_counter()
 
 auth = redditInstance.initiate_instance()
 
-postDF = grabPosts.post_and_timestamps(auth, grabPosts.reddit_input_lists)
+postDF = grabPosts.post_and_timestamps(auth)
 # fileName = input("Enter file name: ")
 
 
