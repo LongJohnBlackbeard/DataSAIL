64d74a9c809079aea6b4034d3522787b432017e8
Daniel Tujo <tujo1992@gmail.com>
2021-05-16 21:19:12 -0500

    testing multi-processing v12

--- scrap/multi-thread_hist_finance/main.py
+++ scrap/multi-thread_hist_finance/main.py
@@ -52,7 +52,7 @@ def data_grab_send(ticker):
         # reset index of the pd
         daily_data = daily_data.reset_index()
         # creates a date range to grab data and fill in missing dates
-        start_date = datetime(2021, 5, 15)
+        start_date = datetime(2021, 5, 16)
         end_date = datetime(2000, 1, 1)
         dateRange = pd.date_range(start=end_date, end=start_date, freq='D')[::-1]
         # sends daterange to pd and resets index
